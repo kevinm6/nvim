bf83117044eea8f939cb8f536dcd83e843c8be86
kevinm6 <72861758+kevinm6@users.noreply.github.com>
2021-12-13 19:40:16 +0100

    Better management of config files

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -14,6 +14,14 @@ if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
   vim.cmd [[packadd packer.nvim]]
 end
 
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]])
+
 -- Section: PLUGINS {
 	local status_ok, packer = pcall(require, "packer")
 	if not status_ok then
---
+++ lua/user/treesitter/init.lua
@@ -0,0 +1,49 @@
+-------------------------------------
+-- File: init.lua
+-- Description: 
+-- Author: Kevin
+-- Source: https://github.com/kevinm6/nvim/blob/nvim/lua/user/treesitter/init.lua
+-- Last Modified: 13/12/21 - 19:33
+-------------------------------------
+
+
+local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+if not status_ok then
+	return
+end
+
+configs.setup({
+	ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+	sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
+	ignore_install = { "" }, -- List of parsers to ignore installing
+	highlight = {
+		enable = true, -- false will disable the whole extension
+		disable = { "" }, -- list of language that will be disabled
+		additional_vim_regex_highlighting = true,
+	},
+	indent = { enable = true, disable = { "python" } },
+	context_commentstring = {
+		enable = true,
+		enable_autocmd = false,
+	},
+	autotag = {
+		enable = true,
+		disable = { "xml" },
+	},
+	ignore_install = { "beancount", "bibtex", "c_sharp", "clojure", "cmake", "comment", "commonlisp", "cuda", "d", "dart", "devicetree", "dockerfile", "dot", "elixir", "elm", "erlang", "fennel", "fish", "fortran", "fusion", "gdscript", "glimmer", "glsl", "godot_resource", "gomod", "graphql", "haskell", "hcl", "heex", "hjson", "javascript", "jsdoc", "julia", "kotlin", "ledger", "llvm", "nix", "ocaml", "ocaml_interface", "ocamllex", "pioasm", "prisma", "pug", "ql", "r", "regex", "rst", "rust", "scala", "scss", "sparql", "supercollider", "surface", "svelte", "teal", "tlaplus", "toml", "tsx", "turtle", "typescript", "verilog", "vue", "yaml", "yang", "zig" },
+	rainbow = {
+		enable = true,
+		colors = {
+			"Gold",
+			"Orchid",
+			"DodgerBlue",
+			-- "Cornsilk",
+			-- "Salmon",
+			-- "LawnGreen",
+		},
+		disable = { "html" },
+	},
+	playground = {
+		enable = true,
+	},
+})
