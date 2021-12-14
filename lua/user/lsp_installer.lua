af026a769214988d786e30c2b728a114a744bbcb
kevinm6 <72861758+kevinm6@users.noreply.github.com>
2021-12-14 16:28:32 +0100

    Change file name

--- lua/user/lsp_installer.lua
+++ lua/user/lsp_installer.lua
@@ -1,8 +1,8 @@
 -------------------------------------
--- File: lspinstaller.lua
+-- File: lsp_installer.lua
 -- Description: nvim-lsp-installer K config
 -- Author: Kevin
--- Source: https://github.com/kevinm6/nvim/blob/nvim/lua/user/lspinstaller.lua
+-- Source: https://github.com/kevinm6/nvim/blob/nvim/lua/user/lsp_installer.lua
 -- Last Modified: 13/12/21 - 16:00
 -------------------------------------
 
@@ -21,16 +21,16 @@
 
 	lsp_installer.on_server_ready(function(server)
 
-	-- Specify the default options which we'll use to setup all servers
-	local default_opts = {
-		on_attach = on_attach,
-	}
+		-- Specify the default options which we'll use to setup all servers
+		local default_opts = {
+			on_attach = on_attach,
+		}
 
-	local server_opts = {}
+		local server_opts = {}
 
-	-- Use the server's custom settings, if they exist, otherwise default to the default options
-	local server_options = server_opts[server.name] and server_opts[server.name]() or default_opts
-		server:setup(server_options)
+		-- Use the server's custom settings, if they exist, otherwise default to the default options
+		local server_options = server_opts[server.name] and server_opts[server.name]() or default_opts
+			server:setup(server_options)
 	end)
 -- nvim-lsp-installer }
 
