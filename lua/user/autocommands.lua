cd0955c3159739de03210e77226f85b4e08279ce
kevinm6 <72861758+kevinm6@users.noreply.github.com>
2021-12-22 14:29:55 +0100

    Add useful files to manage commands and registers and plugin to rename vars

---
+++ lua/user/autocommands.lua
@@ -0,0 +1,52 @@
+ -------------------------------------
+ -- File: autocommands.lua
+ -- Description:
+ -- Author: Kevin
+ -- Source: https://github.com/kevinm6/
+ -- Last Modified: 22/12/21 - 14:04
+ -------------------------------------
+
+vim.cmd [[
+	" AutoCommands
+	augroup _general_settings
+		autocmd!
+		autocmd FileType qf,help,man,lspinfo,spectre_panel nnoremap <silent> <buffer> q :close<CR>
+		autocmd FileType qf set nobuflisted
+		autocmd TextYankPost * silent! lua require('vim.highlight').on_yank{higroup="Search", timeout=300}
+	augroup end
+
+	function! CreateInPreview()
+		let l:filename = input("⟩ Enter filename: ")
+		execute 'splitbelow ' . b:netrw_curdir.'/'.l:filename
+	endf
+
+	au filetype netrw call Netrw_mappings()
+	function! Netrw_mappings()
+		noremap <buffer>% :call CreateInPreview()<cr>
+	endfunction
+
+	" Java
+	augroup jdtls_lsp
+		autocmd!
+		autocmd FileType java lua require'jdtls_setup'.setup()
+	augroup end
+
+	" Markdown
+	augroup _markdown
+		autocmd!
+		autocmd BufNewFile,BufRead *.markdown,*.mdown,*.mkd,*.mkdn,*.md set filetype=markdown 
+	augroup end
+
+	" SQL
+	augroup _sql
+		autocmd!
+		autocmd BufNewFile, BufRead psql* setlocal filetype sql
+	augroup end
+
+	augroup _auto_resize
+		autocmd!
+		autocmd VimResized * tabdo wincmd =
+	augroup end
+
+	command! Scratch lua require'tool'.makeScratch()
+]]
