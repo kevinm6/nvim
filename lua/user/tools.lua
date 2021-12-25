8257d100f793d06dbb714aa00c02de661446b8e8
kevinm6 <72861758+kevinm6@users.noreply.github.com>
2021-12-25 11:11:20 +0100

    Converted to lua

--- lua/user/tools.lua
+++ lua/user/tools.lua
@@ -3,16 +3,15 @@
 -- Description: K utility tools
 -- Author: Kevin
 -- Source: https://github.com/kevinm6/nvim/blob/nvim/lua/user/tools.lua
--- Last Modified: 13/12/21 - 19:12
+-- Last Modified: 22/12/21 - 08:53
 -------------------------------------
 
 local M = {}
 function M.makeScratch()
 	vim.api.nvim_command [[enew]]
-	noswapfile hide enew
-	set.buftype = 'nofile'
-	set.bufhidden = 'hide'
-	set.nobuflisted = true
-	set.swapfile = false
+	vim.opt.buftype = 'nofile'
+	vim.opt.bufhidden = 'hide'
+	vim.opt.nobuflisted = true
+	vim.opt.swapfile = false
 end
 return M
