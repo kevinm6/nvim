1ca7c58b3f3eec88c9b3e470e8ec8f19e20076de
kevinm6 <72861758+kevinm6@users.noreply.github.com>
2021-12-25 11:12:25 +0100

    Move autocommands to dedicated file and set just split right

--- lua/user/prefs.lua
+++ lua/user/prefs.lua
@@ -3,7 +3,7 @@
  -- Description: VimR & NeoVim settings in lua
  -- Author: Kevin
  -- Source: https://github.com/kevinm6/
- -- Last Modified: 20/12/21 - 09:41
+ -- Last Modified: 22/12/21 - 14:15
  -------------------------------------
 
 vim.api.nvim_buf_set_option(0, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
@@ -30,42 +30,7 @@ vim.api.nvim_buf_set_option(0, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 		if $TERM_PROGRAM == "vscode"
 			finish
 		end
-
-		" AutoCommands
-		augroup AutoSaveGroup
-			au!
-			au BufWinLeave,BufLeave,BufWritePost,BufHidden,QuitPre ?* nested silent! mkview!
-			au BufWinEnter ?* silent! loadview
-			au CursorHold, BufEnter * :checktime
-			au TextYankPost * silent! lua vim.highlight.on_yank{higroup="Search", timeout=400}
-		augroup end
-
-		au filetype netrw call Netrw_mappings()
-		function! Netrw_mappings()
-			noremap <buffer>% :call CreateInPreview()<cr>
-		endfunction
-
-		function! CreateInPreview()
-			let l:filename = input("⟩ Enter filename: ")
-			execute 'splitbelow ' . b:netrw_curdir.'/'.l:filename
-		endf
-
-		augroup jdtls_lsp
-			autocmd!
-			autocmd FileType java lua require'jdtls_setup'.setup()
-		augroup end
-
-		" Markdown
-		au BufNewFile,BufRead *.markdown,*.mdown,*.mkd,*.mkdn,*.md setlocal filetype markdown 
-		
-		" SQL
-		au BufNewFile, BufRead psql* setlocal filetype sql
-
-		" Pseudo
-		au BufRead,BufNewFile *.pseudo setlocal filetype pseudo
-
-		command! Scratch lua require'tool'.makeScratch()
-		]], true)
+	]], true)
  -- }
 
 -- Section: Colorscheme
@@ -73,9 +38,8 @@ vim.api.nvim_buf_set_option(0, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 -- }
 
 	local options = {
-	-- Section: MOUSE {
+	-- Section: MOUSE
 		mouse = 'a',
-	-- }
 
 	-- Section: GRAPHIC
 		termguicolors = true,
@@ -91,31 +55,28 @@ vim.api.nvim_buf_set_option(0, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 		cmdheight = 2,	-- #lines for vim for commands/logs
 		pumheight = 16, -- popup menu height
 		pumblend = 8, -- popup menu transparency {0..100}
-		splitbelow = true, -- split below in horizontal split
+		-- splitbelow = true, -- split below in horizontal split
 		splitright = true, -- split right in vertical split
 		updatetime = 300, -- set a low updatetime for better UX even w/ CoC
 		listchars = { tab = "⇥ ", eol = "↲", trail = "~" },
 		timeoutlen = 240,
 		ttimeoutlen = 50,
 		completeopt = { "menu", "menuone", "noselect"},
-	-- }
 
-	-- Section: INDENTATION {
+	-- Section: INDENTATION
 		smartindent = true, -- enable smart indentation
 		tabstop = 2,
 		softtabstop = -1,
 		shiftwidth = 0, -- set tabs
-	-- }
 
-	-- Section: FOLDING {
+	-- Section: FOLDING
 		wrap = true, -- Wrap long lines showing a linebreak
 		foldenable = true, -- enable code folding
 		foldmethod = 'syntax',
 		diffopt = { 'internal', 'filler', 'closeoff', 'vertical' },
 		foldcolumn = 'auto',	-- Add a bit extra margin to the Left
-	-- }
 
-	-- Section: FILE MANAGEMENT {
+	-- Section: FILE MANAGEMENT
 		autowrite = true, -- write files
 		autowriteall = true, -- write files on exit or other changes
 		autochdir = true, -- auto change directory of explore
@@ -123,14 +84,12 @@ vim.api.nvim_buf_set_option(0, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 		backup = false, -- disable backups
 		swapfile = false, -- disable swaps
 		undodir = os.getenv("HOME") .. "/.cache/nvim/tmp/undo",
-	-- }
 
-	-- Section: SEARCH {
+	-- Section: SEARCH
 		smartcase = true, -- smart case for search
-	-- }
 	}
 
-	-- Setting value
+	-- Sets all options to desired value
 	for k, v in pairs(options) do
 		vim.opt[k] = v
 	end
