c3967ad1d2cf4dc69b3f151b7602b654bc9b5ce9
kevinm6 <72861758+kevinm6@users.noreply.github.com>
2022-01-11 19:48:31 +0100

    add maps for toggleterm

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -14,56 +14,48 @@ end
 
 local setup = {
   plugins = {
-    marks = true, -- shows a list of your marks on ' and `
-    registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+    marks = true,
+    registers = true,
     spelling = {
-      enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
-      suggestions = 26, -- how many suggestions should be shown in the list?
+      enabled = true,
+      suggestions = 26,
     },
     presets = {
-      operators = false, -- adds help for operators like d, y, ... and registers them for motion / text object completion
-      motions = true, -- adds help for motions
-      text_objects = true, -- help for text objects triggered after entering an operator
-      windows = true, -- default bindings on <c-w>
-      nav = true, -- misc bindings to work with windows
-      z = true, -- bindings for folds, spelling and others prefixed with z
-      g = true, -- bindings for prefixed with g
+      operators = false,
+      motions = true,
+      text_objects = true,
+      windows = true,
+      nav = true,
+      z = true,
+      g = true,
     },
   },
-  -- add operators that will trigger motion and text object completion
-  -- to enable all native operators, set the preset / operators plugin above
-  -- operators = { gc = "Comments" },
   icons = {
-    breadcrumb = ">", -- symbol used in the command line area that shows your active key combo
-    separator = "➜", -- symbol used between a key and it's label
-    group = "+ ", -- symbol prepended to a group
+    breadcrumb = ">",
+    separator = "➜",
+    group = "+ ",
   },
   popup_mappings = {
-    scroll_down = "<c-d>", -- binding to scroll down inside the popup
-    scroll_up = "<c-u>", -- binding to scroll up inside the popup
+    scroll_down = "<c-d>",
+    scroll_up = "<c-u>",
   },
   window = {
-    border = "shadow", -- none, single, double, shadow
-    position = "bottom", -- bottom, top
-    margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
-    padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
+    border = "shadow",
+    position = "bottom",
+    margin = { 1, 0, 1, 0 },
+    padding = { 2, 2, 2, 2 },
     winblend = 8,
   },
   layout = {
-    height = { min = 4, max = 24 }, -- min and max height of the columns
-    width = { min = 20, max = 46 }, -- min and max width of the columns
-    spacing = 3, -- spacing between columns
-    align = "center", -- align columns left, center or right
+    height = { min = 4, max = 24 },
+    width = { min = 20, max = 46 },
+    spacing = 3,
+    align = "center",
   },
-  ignore_missing = false, -- enable this to hide mappings for which you didn't specify a label
+  ignore_missing = false,
   hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "<cr>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
-  show_help = true, -- show help message on the command line when the popup is visible
-  -- triggers = "auto", -- automatically setup triggers
-  -- triggers = {"<leader>"} -- or specify a list manually
+  show_help = true,
   triggers_blacklist = {
-    -- list of mode / prefixes that should never be hooked by WhichKey
-    -- this is mostly relevant for key maps that start with a native binding
-    -- most people should not need to change this
     i = { "j", "k" },
     v = { "j", "k" },
   },
@@ -231,12 +223,22 @@ local leader_maps = {
 		},
   },
 
-	["<C-n>"] = {
+	-- NVIMTREE
+	e = {
 		name = "Nvim-Tree",
 		r = { "<cmd>NvimTreeRefresh<cr>", "Refresh Nvim-Tree" },
 		o = { "<cmd>NvimTreeOpen<cr>", "Open Nvim-Tree window" },
 		c = { "<cmd>NvimTreeClose<cr>", "Close Nvim-Tree window" },
 		C = { "<cmd>NvimTreeClipboard<cr>", "Show Nvim-Tree clipboard" },
+	},
+
+	-- Terminal
+	T = {
+		name = "Terminal",
+    t = { "<cmd>lua _HTOP_TOGGLE()<cr>", "Htop" },
+    f = { "<cmd>ToggleTerm direction=float<cr>", "Float" },
+    h = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", "Horizontal" },
+    v = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", "Vertical" },
 	},
 
   -- Deletion
@@ -261,7 +263,19 @@ local nosilent_maps = {
   },
 }
 
+local vopts = {
+  mode = "v", -- VISUAL mode
+  prefix = "<leader>",
+  buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
+  silent = true, -- use `silent` when creating keymaps
+  noremap = true, -- use `noremap` when creating keymaps
+  nowait = true, -- use `nowait` when creating keymaps
+}
+local vmappings = {
+  ["/"] = { "<ESC><CMD>lua require(\"Comment.api\").toggle_linewise_op(vim.fn.visualmode())<CR>", "Comment" },
+}
 
 which_key.setup(setup)
 which_key.register(leader_maps, opts)
 which_key.register(nosilent_maps, nosilent_opts)
+which_key.register(vmappings, vopts)
