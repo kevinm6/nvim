acc731202e1a8a1462c89d804d77edf3a855096d
kevinm6 <72861758+kevinm6@users.noreply.github.com>
2022-01-11 19:49:24 +0100

    Update autocommands

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -2,16 +2,14 @@
  -- File: autocommands.lua
  -- Description:
  -- Author: Kevin
- -- Source: https://github.com/kevinm6/
- -- Last Modified: 10/01/22 - 09:39
+ -- Source: https://github.com/kevinm6/nvim/blob/nvim/lua/user/autocommands.lua
+ -- Last Modified: 11/01/22 - 09:55
  -------------------------------------
 
 vim.cmd [[
-	" AutoCommands
 	augroup _general_settings
 		autocmd!
 		autocmd FileType qf,help,man,lspinfo,spectre_panel,Scratch nnoremap <silent> <buffer> q :close<CR>
-		autocmd FileType qf set nobuflisted
 		autocmd TextYankPost * silent! lua require('vim.highlight').on_yank{higroup="Search", timeout=300}
 	augroup end
 
@@ -19,6 +17,8 @@ vim.cmd [[
 	augroup _markdown
 		autocmd!
 		autocmd BufNewFile,BufRead *.markdown,*.mdown,*.mkd,*.mkdn,*.md set filetype=markdown 
+		autocmd FileType markdown setlocal wrap
+		autocmd FileType markdown setlocal spell
 	augroup end
 
 	" SQL
@@ -28,11 +28,11 @@ vim.cmd [[
 	augroup end
 
 	augroup _auto_resize
-		autocmd!
-		autocmd VimResized * tabdo wincmd =
-	augroup end
-	
-	augroup packer_user_config
+    autocmd!
+    autocmd VimResized * tabdo wincmd = 
+  augroup end
+
+	augroup _packer_user_config
 		autocmd!
 		autocmd BufWritePost plugins.lua source <afile> | PackerCompile
 	augroup end
