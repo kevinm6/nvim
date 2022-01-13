691c99e39404a837bdb971c69a6e5ba8885b4f4c
kevinm6 <72861758+kevinm6@users.noreply.github.com>
2022-01-13 14:28:50 +0100

    Rearrange order of sourcing files

--- init.lua
+++ init.lua
@@ -3,7 +3,7 @@
 -- Description: NeoVim K configuration (Lua)
 -- Author: Kevin
 -- Source: https://github.com/kevinm6/nvim/blob/nvim/init.lua
--- Last Modified: 11/01/22 - 19:33
+-- Last Modified: 13/01/22 - 14:20
 --------------------------------------
 
 
@@ -16,25 +16,25 @@
 	end
 -- }
 
--- Section: Other config files to source {
+-- Section: Config Files to source {
 	require "user.prefs"
-	require "user.vars"
-	require "user.autocommands"
-	require "user.bufferline"
-	require "user.statusline"
-	require "user.plugins"
 	require "user.maps"
+	require "user.plugins"
+	require "user.vars"
 	require "user.cmp"
 	require "user.lsp"
-	require "user.treesitter"
-	require "user.gitsigns"
-	require "user.autopairs"
 	require "user.telescope"
-	require "user.surround"
+	require "user.treesitter"
+	require "user.autopairs"
   require "user.comment"
+	require "user.gitsigns"
+	require "user.nvim-tree"
+	require "user.bufferline"
+	require "user.toggleterm"
+	require "user.whichkey"
+	require "user.autocommands"
+	require "user.statusline"
+	require "user.surround"
 	require "user.renamer"
 	require "user.registers"
-	require "user.nvim-tree"
-	require "user.whichkey"
-	require "user.toggleterm"
 -- }
