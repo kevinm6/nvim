3bb0211eec0bf82e982354e715b44ddd88a9ac7c
kevinm6 <72861758+kevinm6@users.noreply.github.com>
2022-01-17 10:38:05 +0100

    Update prefs.

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -3,7 +3,7 @@
 -- Description: TreeSitter config
 -- Author: Kevin
 -- Source: https://github.com/kevinm6/nvim/blob/nvim/lua/user/treesitter.lua
--- Last Modified: 13/01/22 - 09:54
+-- Last Modified: 17/01/2022 - 10:37
 -------------------------------------
 
 
@@ -15,7 +15,7 @@ end
 configs.setup({
 	ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
 	sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-	ignore_install = { "beancount", "bibtex", "c_sharp", "clojure", "cmake", "comment", "commonlisp", "cuda", "d", "dart", "devicetree", "dockerfile", "dot", "elixir", "elm", "erlang", "fennel", "fish", "fortran", "fusion", "gdscript", "glimmer", "glsl", "godot_resource", "gomod", "graphql", "haskell", "hcl", "heex", "hjson", "javascript", "jsdoc", "julia", "kotlin", "ledger", "llvm", "nix", "ocaml", "ocaml_interface", "ocamllex", "pioasm", "prisma", "pug", "ql", "r", "regex", "rst", "rust", "scala", "scss", "sparql", "supercollider", "surface", "svelte", "teal", "tlaplus", "toml", "tsx", "turtle", "typescript", "verilog", "vue", "yaml", "yang", "zig" },
+	ignore_install = { "beancount", "bibtex", "c_sharp", "clojure", "cmake", "comment", "commonlisp", "cuda", "d", "dart", "devicetree", "dockerfile", "dot", "elixir", "elm", "erlang", "fennel", "fish", "fortran", "fusion", "gdscript", "glimmer", "glsl", "godot_resource", "gomod", "graphql", "haskell", "hcl", "heex", "hjson", "javascript", "jsdoc", "julia", "kotlin", "ledger", "llvm", "nix", "ocaml", "ocaml_interface", "ocamllex", "pioasm", "prisma", "pug", "ql", "r", "regex", "rst", "rust", "scala", "scss", "sparql", "supercollider", "surface", "svelte", "teal", "tlaplus", "toml", "tsx", "turtle", "typescript", "verilog", "vue", "yaml", "yang", "zig", "hocon" },
 	highlight = {
 		enable = true, -- false will disable the whole extension
 		disable = { "" }, -- list of language that will be disabled
