6ff93c27500753a8755852d1a2872d045a337048
kevinm6 <72861758+kevinm6@users.noreply.github.com>
2022-01-18 15:58:31 +0100

    change name for custom augroups & add auto-source init.lua when edited

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -3,12 +3,14 @@
  -- Description: Autocommands config
  -- Author: Kevin
  -- Source: https://github.com/kevinm6/nvim/blob/nvim/lua/user/autocommands.lua
- -- Last Modified: 18/01/2022 - 09:38
+ -- Last Modified: 18/01/2022 - 15:43
  -------------------------------------
 
 vim.cmd [[
-	augroup _general_settings
+	augroup general_settings
 		autocmd!
+		autocmd! BufWritePost $MYVIMRC source $MYVIMRC
+
 		autocmd FileType qf,help,man,lspinfo,Scratch nnoremap <silent> <buffer> q :close<CR>
 		autocmd TextYankPost * silent! lua require('vim.highlight').on_yank{higroup="Search", timeout=320}
 
@@ -19,7 +21,7 @@ vim.cmd [[
 		autocmd BufNewFile, BufRead psql* setlocal filetype sql
 	augroup end
 
-	augroup _auto_resize
+	augroup auto_resize
     autocmd!
     autocmd VimResized * tabdo wincmd = 
   augroup end
