061717d3e659c69a38993d23801d16f8d91db871
kevinm6 <72861758+kevinm6@users.noreply.github.com>
2022-01-27 18:38:59 +0100

    Set languages to keep for treesitter

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -3,7 +3,7 @@
 -- Description: TreeSitter config
 -- Author: Kevin
 -- Source: https://github.com/kevinm6/nvim/blob/nvim/lua/user/treesitter.lua
--- Last Modified: 17/01/2022 - 10:37
+-- Last Modified: 27/01/2022 - 18:35
 -------------------------------------
 
 
@@ -13,9 +13,9 @@ if not status_ok then
 end
 
 configs.setup({
-	ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+	ensure_installed ={ "devicetree", "dot", "c", "bash", "comment", "css", "cpp", "dockerfile", "go", "http", "html", "php", "json", "json5", "java", "lua", "markdown", "latex", "perl", "python", "query", "phpdoc", "regex", "swift", "vim" }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
 	sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-	ignore_install = { "beancount", "bibtex", "c_sharp", "clojure", "cmake", "comment", "commonlisp", "cuda", "d", "dart", "devicetree", "dockerfile", "dot", "elixir", "elm", "erlang", "fennel", "fish", "fortran", "fusion", "gdscript", "glimmer", "glsl", "godot_resource", "gomod", "graphql", "haskell", "hcl", "heex", "hjson", "javascript", "jsdoc", "julia", "kotlin", "ledger", "llvm", "nix", "ocaml", "ocaml_interface", "ocamllex", "pioasm", "prisma", "pug", "ql", "r", "regex", "rst", "rust", "scala", "scss", "sparql", "supercollider", "surface", "svelte", "teal", "tlaplus", "toml", "tsx", "turtle", "typescript", "verilog", "vue", "yaml", "yang", "zig", "hocon" },
+	ignore_install = {},
 	highlight = {
 		enable = true, -- false will disable the whole extension
 		disable = { "" }, -- list of language that will be disabled
