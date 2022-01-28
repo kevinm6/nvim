35f878a69c5d587838c5240e037163c924b2d4a1
kevinm6 <72861758+kevinm6@users.noreply.github.com>
2022-01-28 10:02:36 +0100

    Update parsers

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -3,7 +3,7 @@
 -- Description: TreeSitter config
 -- Author: Kevin
 -- Source: https://github.com/kevinm6/nvim/blob/nvim/lua/user/treesitter.lua
--- Last Modified: 27/01/2022 - 18:35
+-- Last Modified: 28/01/2022 - 10:01
 -------------------------------------
 
 
@@ -13,9 +13,10 @@ if not status_ok then
 end
 
 configs.setup({
-	ensure_installed ={ "devicetree", "dot", "c", "bash", "comment", "css", "cpp", "dockerfile", "go", "http", "html", "php", "json", "json5", "java", "lua", "markdown", "latex", "perl", "python", "query", "phpdoc", "regex", "swift", "vim" }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+	ensure_installed = { "devicetree", "dot", "c", "bash", "comment", "css", "cpp", "dockerfile", "go", "http", "html", "php", "json", "json5", "java", "lua", "markdown", "latex", "perl", "python", "query", "phpdoc", "regex", "swift", "vim" }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
 	sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-	ignore_install = {},
+	ignore_install = { "beancount", "bibtex", "c_sharp", "clojure", "cmake", "commonlisp", "cuda", "d", "dart", "eex", "elixir", "elm", "erlang", "fennel", "foam", "fortran", "fusion", "gdscript", "glimmer", "glsl", "gomod", "gowork", "haskell", "hcl", "heex", "hocon", "julia", "ledger", "ninja", "norg", "ocaml", "ocaml_interface", "ocamllex", "pioasm", "prisma", "pug", "r", "rasi", "rst", "scala", "sparql", "supercollider", "surface", "svelte", "teal", "tlaplus", "toml", "tsx", "turtle", "verilog", "yang", "zig"
+	},
 	highlight = {
 		enable = true, -- false will disable the whole extension
 		disable = { "" }, -- list of language that will be disabled
