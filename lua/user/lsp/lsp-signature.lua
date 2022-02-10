ceaf41fd68a1a30e29680a7a6a29b3529a619ae4
kevinm6 <72861758+kevinm6@users.noreply.github.com>
2022-02-10 10:35:04 +0100

    Update graphic of lsp-signature window

--- lua/user/lsp/lsp-signature.lua
+++ lua/user/lsp/lsp-signature.lua
@@ -38,7 +38,7 @@ local cfg = {
   -- to view the hiding contents
   max_width = 120, -- max_width of signature floating_window, line will be wrapped if exceed max_width
   handler_opts = {
-    border = "rounded", -- double, rounded, single, shadow, none
+    border = "shadow", -- double, rounded, single, shadow, none
   },
 
   always_trigger = false, -- sometime show signature on new line or in middle of parameter can be confusing, set it to false for #58
