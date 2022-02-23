e8d4965b43760929c0d1c603d99b9b9139ab9c2d
kevinm6 <72861758+kevinm6@users.noreply.github.com>
2022-02-23 11:49:23 +0100

    Update spelling

--- lua/user/lsp/settings/ltex.lua
+++ lua/user/lsp/settings/ltex.lua
@@ -3,7 +3,7 @@
 -- Description: ltex server config
 -- Author: Kevin
 -- Source: https://github.com/kevinm6/nvim/blob/nvim/lua/user/lsp/settings/ltex.lua
--- Last Modified: 14/02/2022 - 13:18
+-- Last Modified: 23/02/2022 - 11:48
 -------------------------------------
 
 return {
@@ -25,10 +25,10 @@ return {
 			java = {
 				path = "$JAVA_HOME",
 			},
-			-- dictionary = {
-			-- 	"en-US:[:$NVIMDOTDIR/spell/en-US.utf-8.add]",
-			-- 	"it-IT:[:$NVIMDOTDIR/spell/it-IT.utf-8.add]"
-			-- },
+			dictionary = {
+				["en-US"] = { ":~/.MacDotfiles/nvim/.config/nvim/spell/en.utf-8.txt" },
+				["it-IT"] = { ":~/.MacDotfiles/nvim/.config/nvim/spell/it.utf-8.txt" },
+			},
     },
   },
 }
