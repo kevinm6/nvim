ee98b4445c3b1c3876d7146991511cbe35c6e48c
kevinm6 <72861758+kevinm6@users.noreply.github.com>
2022-02-24 11:00:38 +0100

    code indentation changed

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -21,76 +21,76 @@ require("luasnip.loaders.from_vscode").lazy_load()
 require("luasnip.loaders.from_vscode").load({ paths = { vim.fn.expand("$NVIMDOTDIR/lua/") } })
 
 -- CMP {
-		cmp.setup ({
-			snippet = {
-				expand = function(args)
-					require'luasnip'.lsp_expand(args.body)
-				end,
-			},
-			mapping = {
-				['<C-p>'] = cmp.mapping.select_prev_item(),
-				['<C-n>'] = cmp.mapping.select_next_item(),
-				['<C-d>'] = cmp.mapping.scroll_docs(-4),
-				['<C-f>'] = cmp.mapping.scroll_docs(4),
-				['<C-Space>'] = cmp.mapping.complete(),
-				['<CR>'] = cmp.mapping.confirm {
-					behavior = cmp.ConfirmBehavior.Replace,
-					-- Insert-mode
-					i = cmp.mapping.confirm({ select = true }),
-					-- Command-mode
-					c = cmp.mapping.confirm({ select = false })
-				},
-				['<Tab>'] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_next_item()
-					elseif luasnip.expand_or_jumpable() then
-						luasnip.expand_or_jump()
-					else
-						fallback()
-					end
-				end),
-				['<S-Tab>'] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_prev_item()
-					elseif luasnip.jumpable(-1) then
-						luasnip.jump(-1)
-					else
-						fallback()
-					end
-				end),
-			},
-			sources = {
-				{ name = 'nvim_lsp' },
-				{ name = 'luasnip' },
-				{ name = 'buffer' },
-				{ name = 'path' },
-				{ name = 'cmdline' },
-				-- { name = 'vim-snippets' }
-				-- { name = 'ultisnips' },
-			},
-			confirm_opts = {
+	cmp.setup ({
+		snippet = {
+			expand = function(args)
+				require'luasnip'.lsp_expand(args.body)
+			end,
+		},
+		mapping = {
+			['<C-p>'] = cmp.mapping.select_prev_item(),
+			['<C-n>'] = cmp.mapping.select_next_item(),
+			['<C-d>'] = cmp.mapping.scroll_docs(-4),
+			['<C-f>'] = cmp.mapping.scroll_docs(4),
+			['<C-Space>'] = cmp.mapping.complete(),
+			['<CR>'] = cmp.mapping.confirm {
 				behavior = cmp.ConfirmBehavior.Replace,
-				select = false,
+				-- Insert-mode
+				i = cmp.mapping.confirm({ select = true }),
+				-- Command-mode
+				c = cmp.mapping.confirm({ select = false })
 			},
-			-- documentation = false,
-			documentation = {
-				border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-			},
-			experimental = {
-				ghost_text = true,
-				native_menu = false,
-			},
-		})
+			['<Tab>'] = cmp.mapping(function(fallback)
+				if cmp.visible() then
+					cmp.select_next_item()
+				elseif luasnip.expand_or_jumpable() then
+					luasnip.expand_or_jump()
+				else
+					fallback()
+				end
+			end),
+			['<S-Tab>'] = cmp.mapping(function(fallback)
+				if cmp.visible() then
+					cmp.select_prev_item()
+				elseif luasnip.jumpable(-1) then
+					luasnip.jump(-1)
+				else
+					fallback()
+				end
+			end),
+		},
+		sources = {
+			{ name = 'nvim_lsp' },
+			{ name = 'luasnip' },
+			{ name = 'buffer' },
+			{ name = 'path' },
+			{ name = 'cmdline' },
+			-- { name = 'vim-snippets' }
+			-- { name = 'ultisnips' },
+		},
+		confirm_opts = {
+			behavior = cmp.ConfirmBehavior.Replace,
+			select = false,
+		},
+		-- documentation = false,
+		documentation = {
+			border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+		},
+		experimental = {
+			ghost_text = true,
+			native_menu = false,
+		},
+	})
 
-	-- Use buffer source for `/`
+-- Use buffer source for `/`
 	cmp.setup.cmdline('/', {
 		sources = {
 			{ name = 'buffer' }
 		}
 	})
-	-- }
+-- }
 
-	-- Use cmdline & path source for '/'
+-- Use cmdline & path source for '/'
 	cmp.setup.cmdline(':', {
 		sources = cmp.config.sources({
 			{ name = 'path' }
@@ -98,5 +98,4 @@ require("luasnip.loaders.from_vscode").load({ paths = { vim.fn.expand("$NVIMDOTD
 			{ name = 'cmdline' }
 		})
 	})
-	-- }
--- cmp }
+-- }
