79d9d7499614e627b567581981b52777b0a6864e
kevinm6 <72861758+kevinm6@users.noreply.github.com>
2022-02-25 16:15:44 +0100

    Update icons and add two items to alpha dashboard

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -25,18 +25,20 @@ dashboard.section.header.val = {
 }
 
 dashboard.section.buttons.val = {
-  dashboard.button("f", icons.documents.Files .. " Find file", ":Telescope find_files <CR>"),
-  dashboard.button("e", icons.ui.NewFile .. " New file", ":ene <BAR> startinsert <CR>"),
+  dashboard.button("f", icons.documents.Files .. " Find file", "<cmd>Telescope find_files <CR>"),
+  dashboard.button("e", icons.ui.NewFile .. " New file", "<cmd>ene <BAR> startinsert <CR>"),
   dashboard.button(
     "p",
     icons.git.Repo .. " Find project",
-    ":lua require('telescope').extensions.projects.projects()<CR>"
+    "<cmd>lua require('telescope').extensions.projects.projects()<CR>"
   ),
-  dashboard.button("r", icons.ui.History .. " Recent files", ":Telescope oldfiles <CR>"),
-  dashboard.button("t", icons.ui.List .. " Find text", ":Telescope live_grep <CR>"),
-  dashboard.button("s", icons.ui.SignIn .. " Find Session", ":Telescope sessions save_current=false <CR>"),
-  dashboard.button("c", icons.ui.Gear .. " Config", ":e ~/.config/nvim/init.lua <CR>"),
-  dashboard.button("q", icons.diagnostics.Error .. " Quit", ":qa<CR>"),
+  dashboard.button("p", icons.ui.Download .. " Packer", "<cmd>WhichKey <leader>p <CR>"),
+  dashboard.button("g", " Git", "<cmd>WhichKey <leader>g <CR>"),
+  dashboard.button("r", icons.ui.History .. " Recent files", "<cmd>Telescope oldfiles <CR>"),
+  dashboard.button("t", icons.ui.List .. " Find text", "<cmd>Telescope live_grep <CR>"),
+  dashboard.button("s", icons.ui.SignIn .. " Find Session", "<cmd>Telescope sessions save_current=false <CR>"),
+  dashboard.button("c", icons.ui.Gear .. " Config", "<cmd>e ~/.config/nvim/init.lua <CR>"),
+  dashboard.button("q", icons.diagnostics.Error .. " Quit", "<cmd>qa<CR>"),
 }
 
 local function footer()
