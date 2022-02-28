85b74e26bef1f8ea1eead98f33511bb97d64b758
kevinm6 <72861758+kevinm6@users.noreply.github.com>
2022-02-28 11:12:13 +0100

    Update dashboard config

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -3,7 +3,7 @@
 --	Description: alplha config for Neovim
 --	Author: Kevin
 --	Source: https://github.com/kevinm6/nvim/blob/nvim/lua/user/alpha.lua
---	Last Modified: 28/02/2022 - 08:40
+--	Last Modified: 28/02/2022 - 11:03
 -----------------------------------
 
 
@@ -27,19 +27,19 @@ dashboard.section.header.val = {
 dashboard.section.buttons.val = {
   dashboard.button("f", icons.documents.Files .. " Find file", "<cmd>Telescope find_files <CR>"),
   dashboard.button("e", icons.ui.NewFile .. " New file", "<cmd>ene <BAR> startinsert <CR>"),
-  dashboard.button(
-    "P",
-    icons.git.Repo .. " Find project",
-    "<cmd>lua require('telescope').extensions.projects.projects()<CR>"
-  ),
+  -- dashboard.button(
+  --   "P",
+  --   icons.git.Repo .. " Find project",
+  --   "<cmd>lua require('telescope').extensions.projects.projects()<CR>"
+  -- ),
   dashboard.button("u", icons.ui.Uni .. " University", "<cmd>e $CS <CR>"),
   dashboard.button("p", icons.ui.Download .. " Packer", "<cmd>WhichKey <leader>p <CR>"),
   dashboard.button("l", icons.ui.List .. " LspInstaller", "<cmd>LspInstallInfo <CR>"),
   dashboard.button("g", " Git", "<cmd>WhichKey <leader>g <CR>"),
   dashboard.button("r", icons.ui.History .. " Recent files", "<cmd>Telescope oldfiles <CR>"),
   dashboard.button("t", icons.ui.List .. " Find text", "<cmd>Telescope live_grep <CR>"),
-  dashboard.button("s", icons.ui.SignIn .. " Find Session", "<cmd>Telescope sessions save_current=false <CR>"),
-  dashboard.button("c", icons.ui.Gear .. " Config", "<cmd>e ~/.config/nvim/init.lua <CR>"),
+  -- dashboard.button("s", icons.ui.SignIn .. " Find Session", "<cmd>Telescope sessions save_current=false <CR>"),
+  dashboard.button("c", icons.ui.Gear .. " Config", "<cmd>cd $NVIMDOTDIR <CR> <BAR> <cmd>e $NVIMDOTDIR/init.lua <CR>"),
   dashboard.button("q", icons.diagnostics.Error .. " Quit", "<cmd>qa<CR>"),
 }
 
