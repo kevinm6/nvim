40e87de7d710c6ef0ec9ea32abe117caa655289d
kevinm6 <72861758+kevinm6@users.noreply.github.com>
2022-03-01 13:36:33 +0100

    Add shortcut to modify plugin file

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -3,7 +3,7 @@
 --	Description: alplha config for Neovim
 --	Author: Kevin
 --	Source: https://github.com/kevinm6/nvim/blob/nvim/lua/user/alpha.lua
---	Last Modified: 28/02/2022 - 11:03
+--	Last Modified: 01/03/2022 - 13:36
 -----------------------------------
 
 
@@ -33,7 +33,8 @@ dashboard.section.buttons.val = {
   --   "<cmd>lua require('telescope').extensions.projects.projects()<CR>"
   -- ),
   dashboard.button("u", icons.ui.Uni .. " University", "<cmd>e $CS <CR>"),
-  dashboard.button("p", icons.ui.Packer .. " Packer", "<cmd>WhichKey <leader>p <CR>"),
+  dashboard.button("p", icons.ui.Packer .. " Plugin config", "<cmd>e $NVIMDOTDIR/lua/user/plugins.lua<CR>"),
+  dashboard.button("P", icons.ui.Packer .. " Packer", "<cmd>WhichKey <leader>p <CR>"),
   dashboard.button("l", icons.ui.List .. " LspInstaller", "<cmd>LspInstallInfo <CR>"),
   dashboard.button("g", icons.ui.Git .. " Git", "<cmd>WhichKey <leader>g <CR>"),
   dashboard.button("r", icons.ui.History .. " Recent files", "<cmd>Telescope oldfiles <CR>"),
