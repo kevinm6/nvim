b96fc394705c1faa30844b1a2734d876f8923491
kevinm6 <72861758+kevinm6@users.noreply.github.com>
2022-03-01 20:46:23 +0100

    Git file now can be closed w/ 'q' key

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -3,13 +3,13 @@
  -- Description: Autocommands config
  -- Author: Kevin
  -- Source: https://github.com/kevinm6/nvim/blob/nvim/lua/user/autocommands.lua
- -- Last Modified: 19/01/2022 - 10:50
+ -- Last Modified: 01/03/2022 - 20:37
  -------------------------------------
 
 vim.cmd [[
 	augroup general_settings
 		autocmd!
-		autocmd FileType qf,help,man,lspinfo,Scratch nnoremap <silent> <buffer> q :close<CR>
+		autocmd FileType qf,help,man,lspinfo,Scratch,git nnoremap <silent> <buffer> q :close<CR>
 		autocmd TextYankPost * silent! lua require('vim.highlight').on_yank{higroup="Visual", timeout=320}
 
 	" Markdown
