6589a9811499950a7c24a6a2ddd5f24be36b8d9e
kevinm6 <72861758+kevinm6@users.noreply.github.com>
2022-03-08 16:04:39 +0100

    Change order

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -88,13 +88,17 @@ end
 		use "rcarriga/nvim-notify"
 
 		-- autocompletion
-		use "hrsh7th/nvim-cmp"
+		use "hrsh7th/cmp-nvim-lsp"
 		use "hrsh7th/cmp-buffer"
 		use "hrsh7th/cmp-path"
 		use "hrsh7th/cmp-cmdline"
-		use "hrsh7th/cmp-nvim-lsp"
+		use "hrsh7th/nvim-cmp"
 		use "hrsh7th/cmp-nvim-lua"
 		use "saadparwaiz1/cmp_luasnip"
+
+		-- snippets
+		use "L3MON4D3/LuaSnip"
+		use "rafamadriz/friendly-snippets"
 
 		-- coding helper
 		use "Mephistophiles/surround.nvim"
@@ -125,9 +129,6 @@ end
 		use "tpope/vim-fugitive"
 		use "lewis6991/gitsigns.nvim"
 
-		-- snippets
-		use "L3MON4D3/LuaSnip"
-		use "rafamadriz/friendly-snippets"
 
 		-- lsp
 		use "neovim/nvim-lspconfig"
