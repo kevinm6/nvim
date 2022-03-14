7342a2e14bfa95a7f4db7cfa13e488dc632f57ca
kevinm6 <72861758+kevinm6@users.noreply.github.com>
2022-03-14 08:58:14 +0100

    Add filetypes to be ignored

--- lua/user/illuminate.lua
+++ lua/user/illuminate.lua
@@ -3,12 +3,13 @@
 --	Description: illuminate plugin config
 --	Author: Kevin
 --	Source: https://github.com/kevinm6/nvim/blob/nvim/lua/user/illuminate.lua
---	Last Modified: 07/03/2022 - 20:59
+--	Last Modified: 14/03/2022 - 08:57
 -----------------------------------
+
 
 -- vim.g.Illuminate_delay = 0
 -- vim.g.Illuminate_highlightUnderCursor = 0
--- vim.g.Illuminate_ftblacklist = {'html'}
-vim.api.nvim_set_keymap('n', '<a-n>', '<cmd>lua require"illuminate".next_reference{wrap=true}<cr>', {noremap=true})
-vim.api.nvim_set_keymap('n', '<a-p>', '<cmd>lua require"illuminate".next_reference{reverse=true,wrap=true}<cr>', {noremap=true})
+vim.g.Illuminate_ftblacklist = { "alpha", "Scratch" }
+vim.api.nvim_set_keymap("n", "<A-n>", '<cmd>lua require("illuminate").next_reference{wrap=true}<cr>', {noremap=true})
+vim.api.nvim_set_keymap("n", "<A-p>", '<cmd>lua require("illuminate").next_reference{reverse=true,wrap=true}<cr>', {noremap=true})
 
