5228362998ec5b19244e05eec1a810925bf1d751
kevinm6 <72861758+kevinm6@users.noreply.github.com>
2022-03-15 10:52:49 +0100

    Set config for sql language server (sqlls)

--- lua/user/vars.lua
+++ lua/user/vars.lua
@@ -3,57 +3,59 @@
  -- Description: NeoVim & VimR global vars
  -- Author: Kevin
  -- Source: https://github.com/kevinm6/nvim/blob/nvim/lua/vars.lua
- -- Last Modified: 04/03/2022 - 18:54
+ -- Last Modified: 15/03/2022 - 10:16
  -------------------------------------
 
 
--- Section: Python {
+-- Python
 	vim.g.python3_host_prog = "/usr/local/bin/python3.9"
 -- }
 
 
--- Section: Database {
-	vim.g.sql_type_default = 'postgresql'
-	vim.g.omni_sql_no_default_maps = 1
+-- Database
+vim.g.sql_type_default = 'postgresql'
+vim.g.omni_sql_no_default_maps = 1
 
-	vim.g.dbs = {
-		imdb = 'postgres://:@localhost/imdb',
-		lezione = 'postgres://:@localhost/lezione'
-	}
--- }
-
-
--- Section: Markdown {
-	vim.g.markdown_fenced_languages = {
-		'html',
-		'python',
-		'zsh',
-		'java',
-		'c', 'C',
-		'bash=sh',
-		'json',
-		'xml',
-		'vim',
-		'help',
-		'javascript', 'js=javascript',
-		'css',
-		'changelog',
-		'cpp',
-		'pseudocode',
-		'php',
-		'sql'
-	}
-
-	vim.g.markdown_folding = 1
-	vim.g.rmd_include_html = 1
-
-	vim.g.glow_binary_path = vim.env.HOME .. "/.local/bin"
--- }
+vim.g.dbs = {
+	imdb = 'postgres://:@localhost/imdb',
+	lezione = 'postgres://:@localhost/lezione'
+}
+vim.g.LanguageClient_serverCommands = {
+	["sql"] = {
+		 "sql-language-server", "up", '--method', 'stdio'
+	},
+}
 
 
--- Section: SESSION {
-	vim.g.session_autosave = 'yes'
-	vim.g.session_autoload = 'yes'
-  vim.g.session_default_to_last = 1
--- }
+-- Markdown
+vim.g.markdown_fenced_languages = {
+	'html',
+	'python',
+	'zsh',
+	'java',
+	'c', 'C',
+	'bash=sh',
+	'json',
+	'xml',
+	'vim',
+	'help',
+	'javascript', 'js=javascript',
+	'css',
+	'changelog',
+	'cpp',
+	'pseudocode',
+	'php',
+	'sql'
+}
+
+vim.g.markdown_folding = 1
+vim.g.rmd_include_html = 1
+
+vim.g.glow_binary_path = vim.env.HOME .. "/.local/bin"
+
+
+-- Session
+vim.g.session_autosave = 'yes'
+vim.g.session_autoload = 'yes'
+vim.g.session_default_to_last = 1
 
