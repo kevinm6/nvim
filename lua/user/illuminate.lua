6b62a24ed61ce83533065555a9713b88cd35f147
kevinm6 <72861758+kevinm6@users.noreply.github.com>
2022-03-21 11:02:42 +0100

    Add nvim-tree to disabled filetypes

--- lua/user/illuminate.lua
+++ lua/user/illuminate.lua
@@ -9,7 +9,7 @@
 
 -- vim.g.Illuminate_delay = 0
 -- vim.g.Illuminate_highlightUnderCursor = 0
-vim.g.Illuminate_ftblacklist = { "alpha", "Scratch" }
+vim.g.Illuminate_ftblacklist = { "alpha", "Scratch", "NvimTree" }
 vim.api.nvim_set_keymap("n", "<a-n>", '<cmd>lua require("illuminate").next_reference{wrap=true}<cr>', {noremap=true})
 vim.api.nvim_set_keymap("n", "<a-p>", '<cmd>lua require("illuminate").next_reference{reverse=true,wrap=true}<cr>', {noremap=true})
 
