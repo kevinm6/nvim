8f6166028a6cf2c49a900b4375a6165798af691e
kevinm6 <72861758+kevinm6@users.noreply.github.com>
2022-03-23 21:40:05 +0100

    Add TODO

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -3,9 +3,11 @@
  -- Description: Autocommands config
  -- Author: Kevin
  -- Source: https://github.com/kevinm6/nvim/blob/nvim/lua/user/autocommands.lua
- -- Last Modified: 21/03/2022 - 11:37
+ -- Last Modified: 23/03/2022 - 18:44
  -------------------------------------
 
+
+-- TODO: change to lua autocmd when it will be available
 
 vim.cmd [[
 	augroup _general_settings
@@ -32,11 +34,6 @@ vim.cmd [[
     autocmd!
     autocmd VimEnter * hi link illuminatedWord LspReferenceText
   augroup END
-
-	augroup _packer_user_config
-		autocmd!
-		autocmd BufWritePost plugins.lua source <afile> | PackerCompile
-	augroup end
 
 	augroup auto_resize
     autocmd!
