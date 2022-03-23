cced9956deb875439cefcddd87aded584bdc042f
kevinm6 <72861758+kevinm6@users.noreply.github.com>
2022-03-23 21:41:13 +0100

    Improvements

--- lua/user/illuminate.lua
+++ lua/user/illuminate.lua
@@ -10,6 +10,6 @@
 -- vim.g.Illuminate_delay = 0
 -- vim.g.Illuminate_highlightUnderCursor = 0
 vim.g.Illuminate_ftblacklist = { "alpha", "Scratch", "NvimTree" }
-vim.api.nvim_set_keymap("n", "<a-n>", '<cmd>lua require("illuminate").next_reference{wrap=true}<cr>', {noremap=true})
-vim.api.nvim_set_keymap("n", "<a-p>", '<cmd>lua require("illuminate").next_reference{reverse=true,wrap=true}<cr>', {noremap=true})
+vim.api.nvim_set_keymap("n", "<C-n>", '<cmd>lua require("illuminate").next_reference{wrap=true}<cr>', {noremap=true})
+vim.api.nvim_set_keymap("n", "<C-p>", '<cmd>lua require("illuminate").next_reference{reverse=true,wrap=true}<cr>', {noremap=true})
 
