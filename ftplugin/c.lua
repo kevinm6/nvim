4673fdaca400ab0091e3b5f120e8c8bb54ef8254
kevinm6 <72861758+kevinm6@users.noreply.github.com>
2022-04-03 13:29:28 +0200

    C filetypes useful commands and prefs for "make" and so on

---
+++ ftplugin/c.lua
@@ -0,0 +1,9 @@
+-------------------------------------
+-- File: java.lua
+-- Description: java language server configuration
+-- Author: Kevin
+-- Source: https://github.com/kevinm6/nvim/blob/nvim/ftplugin/java.lua
+-- Last Modified: 12/03/2022 - 18:30
+-------------------------------------
+
+vim.opt.makeprg = "gcc % -o %<"
--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -4,7 +4,7 @@
 -- Author: Kevin
 -- Source: https://github.com/kevinm6/nvim/blob/nvim/lua/user/whichkey.lua
 -- Help-Source: https://github.com/ChristianChiarulli/nvim/lua/user/which_key.lua
--- Last Modified: 26/03/2022 - 21:10
+-- Last Modified: 03/04/2022 - 10:51
 -------------------------------------
 
 local ok, which_key = pcall(require, "which-key")
@@ -41,9 +41,9 @@ local setup = {
   window = {
     border = "shadow",
     position = "bottom",
-    margin = { 1, 0, 1, 0 },
+    margin = { 1, 1, 1, 1 },
     padding = { 2, 2, 2, 2 },
-    winblend = 8,
+    winblend = 20,
   },
   layout = {
     height = { min = 4, max = 24 },
@@ -79,36 +79,55 @@ local nosilent_opts = {
 }
 
 
-local leader_maps = {
-  ["1"] = { "<cmd>BufferLineGoToBuffer 1<cr>", "Go to Buffer 1" },
-  ["2"] = { "<cmd>BufferLineGoToBuffer 2<cr>", "Go to Buffer 2" },
-  ["3"] = { "<cmd>BufferLineGoToBuffer 3<cr>", "Go to Buffer 3" },
-  ["4"] = { "<cmd>BufferLineGoToBuffer 4<cr>", "Go to Buffer 4" },
-  ["5"] = { "<cmd>BufferLineGoToBuffer 5<cr>", "Go to Buffer 5" },
-  ["6"] = { "<cmd>BufferLineGoToBuffer 6<cr>", "Go to Buffer 6" },
-  ["7"] = { "<cmd>BufferLineGoToBuffer 7<cr>", "Go to Buffer 7" },
-  ["8"] = { "<cmd>BufferLineGoToBuffer 8<cr>", "Go to Buffer 8" },
-  ["9"] = { "<cmd>BufferLineGoToBuffer 9<cr>", "Go to Buffer 9" },
+local noLeader_opts = {
+  mode = "n", -- NORMAL mode
+  prefix = "",
+  buffer = nil,
+  silent = false,
+  noremap = true,
+  nowait = true,
+}
 
+local nonLeader_maps = {
+  ["<F8>"] = {
+        "<cmd>update<CR> <cmd>! gcc % -o %< <CR> <cmd>NvimTreeRefresh<CR> <cmd>! ./%< <CR>",
+        "Save, Compile & Run"
+      },
+}
+
+local leader_maps = {
+  ["1"] = { "<cmd>BufferLineGoToBuffer 1<CR>", "Go to Buffer 1" },
+  ["2"] = { "<cmd>BufferLineGoToBuffer 2<CR>", "Go to Buffer 2" },
+  ["3"] = { "<cmd>BufferLineGoToBuffer 3<CR>", "Go to Buffer 3" },
+  ["4"] = { "<cmd>BufferLineGoToBuffer 4<CR>", "Go to Buffer 4" },
+  ["5"] = { "<cmd>BufferLineGoToBuffer 5<CR>", "Go to Buffer 5" },
+  ["6"] = { "<cmd>BufferLineGoToBuffer 6<CR>", "Go to Buffer 6" },
+  ["7"] = { "<cmd>BufferLineGoToBuffer 7<CR>", "Go to Buffer 7" },
+  ["8"] = { "<cmd>BufferLineGoToBuffer 8<CR>", "Go to Buffer 8" },
+  ["9"] = { "<cmd>BufferLineGoToBuffer 9<CR>", "Go to Buffer 9" },
+
+  b = {
+    "<cmd>lua require('telescope.builtin').buffers(require('telescope.themes').get_dropdown{previewer = false})<CR>",
+    "Buffers",
+  },
   w = { "<cmd>update!<CR>", "Save" },
   h = { "<cmd>nohlsearch<CR>", "No Highlight" },
   c = { "<cmd>Bdelete!<CR>", "Close Buffer" },
-	z = { ":update<cr>:Bdelete<cr>:bnext<cr>", "Save and Close Buffer" },
-	q = { "<cmd>quit<cr>", "Quit" },
-	a = { "<cmd>Alpha<cr>", "Alpha Dashboard"},
-	Z = { "<cmd>ZenMode<cr>", "Zen"},
-  ["%"] = { "<cmd>source %<cr>", "Save current buffer" },
+	z = { "<cmd>update<CR> <cmd>Bdelete<cr> <cmd>bnext<cr>", "Save and Close Buffer" },
+	q = { "<cmd>quit<CR>", "Quit" },
+	a = { "<cmd>Alpha<CR>", "Alpha Dashboard"},
+	Z = { "<cmd>ZenMode<CR>", "Zen"},
 
 	f = {
 		name = "Find (Telescope)",
 		f = {
-			"<cmd>lua require('telescope.builtin').find_files(require('telescope.themes').get_dropdown{previewer = false})<cr>",
+			"<cmd>lua require('telescope.builtin').find_files(require('telescope.themes').get_dropdown{previewer = false})<CR>",
 			"Find files",
 		},
-		F = { "<cmd>Telescope live_grep theme=ivy<cr>", "Find Text (live grep)" },
-		P = { "<cmd>Telescope projects<cr>", "Projects" },
+		F = { "<cmd>Telescope live_grep theme=ivy<CR>", "Find Text (live grep)" },
+		P = { "<cmd>Telescope projects<CR>", "Projects" },
 		b = {
-			"<cmd>lua require('telescope.builtin').buffers(require('telescope.themes').get_dropdown{previewer = false})<cr>",
+			"<cmd>lua require('telescope.builtin').buffers(require('telescope.themes').get_dropdown{previewer = false})<CR>",
 			"Buffers",
 		},
 	},
@@ -121,78 +140,78 @@ local leader_maps = {
 	},
 
   -- Renamer
-	R = { '<cmd>lua require("renamer").rename()<cr>', "Renamer" },
+	r = { '<cmd>lua require("renamer").rename()<CR>', "Renamer" },
 
   -- Packer
   p = {
     name = "Packer",
-    C = { "<cmd>PackerCompile<cr>", "Compile" },
-    c = { "<cmd>PackerClean<cr>", "Clean" },
-    i = { "<cmd>PackerInstall<cr>", "Install" },
-    S = { "<cmd>PackerSync<cr>", "Sync" },
-    s = { "<cmd>PackerStatus<cr>", "Status" },
-    u = { "<cmd>PackerUpdate<cr>", "Update" },
+    C = { "<cmd>PackerCompile<CR>", "Compile" },
+    c = { "<cmd>PackerClean<CR>", "Clean" },
+    i = { "<cmd>PackerInstall<CR>", "Install" },
+    S = { "<cmd>PackerSync<CR>", "Sync" },
+    s = { "<cmd>PackerStatus<CR>", "Status" },
+    u = { "<cmd>PackerUpdate<CR>", "Update" },
   },
 
   -- Git
 	g =  {
 		name = "Git",
-		s = { "<cmd>Git status<cr>", "Git status in cmdline" },
-		g = { "<cmd>Git<cr>", "Git summary" },
-		A = { "<cmd>Git add .<cr>", "Git add folder" },
-		d = { "<cmd>Git df %<cr>", "Git diff current file" },
-		D = { "<cmd>Git df<cr>", "Git diff" },
-		p = { "<cmd>Git push<cr>", "Git push" },
+		s = { "<cmd>Git status<CR>", "Git status in cmdline" },
+		g = { "<cmd>Git<CR>", "Git summary" },
+		A = { "<cmd>Git add .<CR>", "Git add folder" },
+		d = { "<cmd>Git df %<CR>", "Git diff current file" },
+		D = { "<cmd>Git df<CR>", "Git diff" },
+		p = { "<cmd>Git push<CR>", "Git push" },
 	},
 
   -- Telescope & Gitsigns
 	G = {
     name = "GitSigns & Telescope-git",
-    j = { "<cmd>lua require 'gitsigns'.next_hunk()<cr>", "Next Hunk" },
-    k = { "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", "Prev Hunk" },
-    l = { "<cmd>GitBlameToggle<cr>", "Blame" },
-    g = { "<cmd>lua _LAZYGIT_TOGGLE<cr>", "Lazygit" },
-    p = { "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", "Preview Hunk" },
-    r = { "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", "Reset Hunk" },
-    R = { "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", "Reset Buffer" },
-    s = { "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", "Stage Hunk" },
+    j = { "<cmd>lua require 'gitsigns'.next_hunk()<CR>", "Next Hunk" },
+    k = { "<cmd>lua require 'gitsigns'.prev_hunk()<CR>", "Prev Hunk" },
+    l = { "<cmd>GitBlameToggle<CR>", "Blame" },
+    g = { "<cmd>lua _LAZYGIT_TOGGLE()<CR>", "Lazygit" },
+    p = { "<cmd>lua require 'gitsigns'.preview_hunk()<CR>", "Preview Hunk" },
+    r = { "<cmd>lua require 'gitsigns'.reset_hunk()<CR>", "Reset Hunk" },
+    R = { "<cmd>lua require 'gitsigns'.reset_buffer()<CR>", "Reset Buffer" },
+    s = { "<cmd>lua require 'gitsigns'.stage_hunk()<CR>", "Stage Hunk" },
     u = {
-      "<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>",
+      "<cmd>lua require 'gitsigns'.undo_stage_hunk()<CR>",
       "Undo Stage Hunk",
     },
-    o = { "<cmd>Telescope git_status<cr>", "Open changed file" },
-    b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
-    c = { "<cmd>Telescope git_commits<cr>", "Checkout commit" },
-    d = { "<cmd>Gitsigns diffthis HEAD<cr>", "Diff", },
+    o = { "<cmd>Telescope git_status<CR>", "Open changed file" },
+    b = { "<cmd>Telescope git_branches<CR>", "Checkout branch" },
+    c = { "<cmd>Telescope git_commits<CR>", "Checkout commit" },
+    d = { "<cmd>Gitsigns diffthis HEAD<CR>", "Diff", },
 	},
 
   -- Skeletons
 	S = {
     name = "Skeletons",
 		h = {
-			"<cmd>1-read $NVIMDOTDIR/snippets/skeleton.html<cr>3jf>a",
+			"<cmd>1-read $NVIMDOTDIR/snippets/skeleton.html<CR>3jf>a",
 			"Create Html skeleton"
 		},
 		c = {
-			":1-read $NVIMDOTDIR/snippets/skeleton.c<cr>4ja",
+			":1-read $NVIMDOTDIR/snippets/skeleton.c<CR>4ja",
 			"Create C skeleton"
 		},
-		j = { ":1-read $NVIMDOTDIR/snippets/skeleton.java<cr>2jA<Left><Left><C-r>%<Esc>d2b2jo",
+		j = { ":1-read $NVIMDOTDIR/snippets/skeleton.java<CR>2jA<Left><Left><C-r>%<Esc>d2b2jo",
 			"Create java skeleton"
 		},
 		f = {
       name = "Functions",
 			j = {
-        ":1-read $NVIMDOTDIR/snippets/method.java<cr>6jf(i",
+        ":1-read $NVIMDOTDIR/snippets/method.java<CR>6jf(i",
 			  "Create java function skeleton"
       },
 		},
 		i = {
-			":1-read $NVIMDOTDIR/snippets/skeleton.info<cr>v}gc<Esc>gg<Esc>jA<C-r>%<Esc>4jA<F2><Esc>3kA",
+			":1-read $NVIMDOTDIR/snippets/skeleton.info<CR>v}gc<Esc>gg<Esc>jA<C-r>%<Esc>4jA<F2><Esc>3kA",
 			"Create info skeleton"
 		},
 		m = {
-			":1-read $NVIMDOTDIR/snippets/skeleton.md<cr>A<Space><C-r>%<Esc>Go",
+			":1-read $NVIMDOTDIR/snippets/skeleton.md<CR>A<Space><C-r>%<Esc>Go",
 			"Create markdown skeleton"
 		},
 	},
@@ -200,94 +219,139 @@ local leader_maps = {
   -- Telescope
   t = {
     name = "Telescope",
-		o = { "<cmd>Telescope<cr>", "Open Telescope" },
-    b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
-    c = { "<cmd>Telescope colorscheme<cr>", "Colorscheme" },
-    f = { "<cmd>Telescope find_files<cr>", "Find File" },
-    h = { "<cmd>Telescope help_tags<cr>", "Help" },
-    i = { "<cmd>Telescope media_files<cr>", "Media" },
-    l = { "<cmd>Telescope resume<cr>", "Last Search" },
-    M = { "<cmd>Telescope man_pages<cr>", "Man Pages" },
-    r = { "<cmd>Telescope oldfiles<cr>", "Recent File" },
-    R = { "<cmd>Telescope registers<cr>", "Registers" },
-    k = { "<cmd>Telescope keymaps<cr>", "Keymaps" },
-    C = { "<cmd>Telescope commands<cr>", "Commands" },
-		p = { "<cmd>TSPlaygroundToggle<cr>", "Playground" },
+		o = { "<cmd>Telescope<CR>", "Open Telescope" },
+    b = { "<cmd>Telescope git_branches<CR>", "Checkout branch" },
+    c = { "<cmd>Telescope colorscheme<CR>", "Colorscheme" },
+    f = { "<cmd>Telescope find_files<CR>", "Find File" },
+    H = { "<cmd>Telescope help_tags<CR>", "Help" },
+    i = { "<cmd>Telescope media_files<CR>", "Media" },
+    l = { "<cmd>Telescope resume<CR>", "Last Search" },
+    M = { "<cmd>Telescope man_pages<CR>", "Man Pages" },
+    r = { "<cmd>Telescope oldfiles<CR>", "Recent File" },
+    R = { "<cmd>Telescope registers<CR>", "Registers" },
+    k = { "<cmd>Telescope keymaps<CR>", "Keymaps" },
+    C = { "<cmd>Telescope commands<CR>", "Commands" },
+		p = { "<cmd>TSPlaygroundToggle<CR>", "TS Playground" },
+		h = { "<cmd>TSHighlightUnderCursor<CR>", "TS Highlight" },
   },
 
   -- LSP
   l = {
     name = "LSP",
-		a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action" },
-		d = { "<cmd>TroubleToggle<cr>", "Diagnostics" },
+		a = { "<cmd>lua vim.lsp.buf.code_action()<CR>", "Code Action" },
+		d = { "<cmd>TroubleToggle<CR>", "Diagnostics" },
     w = {
-      "<cmd>Telescope lsp_workspace_diagnostics<cr>",
+      "<cmd>Telescope lsp_workspace_diagnostics<CR>",
       "Workspace Diagnostics",
     },
-    f = { "<cmd>lua vim.lsp.buf.formatting()<cr>", "Format" },
-    F = { "<cmd>LspToggleAutoFormat<cr>", "Toggle Autoformat" },
-    i = { "<cmd>LspInfo<cr>", "Info" },
-    I = { "<cmd>LspInstallInfo<cr>", "Installer Info" },
+    f = { "<cmd>lua vim.lsp.buf.formatting()<CR>", "Format" },
+    F = { "<cmd>LspToggleAutoFormat<CR>", "Toggle Autoformat" },
+    i = { "<cmd>LspInfo<CR>", "Info" },
+    I = { "<cmd>LspInstallInfo<CR>", "Installer Info" },
     j = {
       "<cmd>lua vim.diagnostic.goto_next({buffer=0)<CR>",
       "Next Diagnostic",
     },
     k = {
-      "<cmd>lua vim.diagnostic.goto_prev({buffer=0)<cr>",
+      "<cmd>lua vim.diagnostic.goto_prev({buffer=0)<CR>",
       "Prev Diagnostic",
     },
-    l = { "<cmd>lua vim.lsp.codelens.run()<cr>", "CodeLens Action" },
-    o = { "<cmd>SymbolsOutline<cr>", "Outline" },
-    q = { "<cmd>lua vim.diagnostic.setloclist()<cr>", "Quickfix" },
-    r = { "<cmd>lua vim.lsp.buf.rename()<cr>", "Rename" },
-    R = { "<cmd>TroubleToggle lsp_references<cr>", "References" },
-    s = { "<cmd>Telescope lsp_document_symbols<cr>", "Document Symbols" },
+    l = { "<cmd>lua vim.lsp.codelens.run()<CR>", "CodeLens Action" },
+    o = { "<cmd>SymbolsOutline<CR>", "Outline" },
+    q = { "<cmd>lua vim.diagnostic.setloclist()<CR>", "Quickfix" },
+    r = { "<cmd>lua vim.lsp.buf.rename()<CR>", "Rename" },
+    R = { "<cmd>TroubleToggle lsp_references<CR>", "References" },
+    s = { "<cmd>Telescope lsp_document_symbols<CR>", "Document Symbols" },
     S = {
-      "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>",
+      "<cmd>Telescope lsp_dynamic_workspace_symbols<CR>",
       "Workspace Symbols",
     },
 	},
 
+  F = {
+    name = "Filetypes ops",
+    c = {
+      name = "[ c ]",
+      c = {
+        "<cmd>update<CR> <cmd>!gcc % -o %< <CR> <cmd>NvimTreeRefresh<CR>",
+        "Save & Compile"
+      },
+      r = { "<cmd>! ./%< % <CR>", "Run program" },
+      C = {
+        "<cmd>update<CR> <cmd>!gcc % -o %< <CR> <cmd>NvimTreeRefresh<CR> <cmd>!./%< % <CR>",
+        "Compile & Run"
+      },
+    }
+  },
+
+
 	-- SURROUND
 	s = {
     name = "Surround",
-    ["."] = { "<cmd>lua require('surround').repeat_last()<cr>", "Repeat" },
-    a = { "<cmd>lua require('surround').surround_add(true)<cr>", "Add" },
-    d = { "<cmd>lua require('surround').surround_delete()<cr>", "Delete" },
-    r = { "<cmd>lua require('surround').surround_replace()<cr>", "Replace" },
-    q = { "<cmd>lua require('surround').toggle_quotes()<cr>", "Quotes" },
-    b = { "<cmd>lua require('surround').toggle_brackets()<cr>", "Brackets" },
+    ["."] = { "<cmd>lua require('surround').repeat_last()<CR>", "Repeat" },
+    a = { "<cmd>lua require('surround').surround_add(true)<CR>", "Add" },
+    d = { "<cmd>lua require('surround').surround_delete()<CR>", "Delete" },
+    r = { "<cmd>lua require('surround').surround_replace()<CR>", "Replace" },
+    q = { "<cmd>lua require('surround').toggle_quotes()<CR>", "Quotes" },
+    b = { "<cmd>lua require('surround').toggle_brackets()<CR>", "Brackets" },
   },
 
 	-- NVIMTREE
-	E = { "<cmd>Neotree toggle reveal_force_cwd<cr>", "Neo-Tree" },
-	e = {
-		name = "NeoTree",
-		r = { "<cmd>NeoTreeRefresh<cr>", "Refresh NeoTree" },
-		o = { "<cmd>Neotree show<cr>", "Open NeoTree window" },
-		t = { "<cmd>Neotree toggle show<cr>", "Toggle NeoTree" },
-		f = { "<cmd>Neotree toggle float reveal<cr>", "Toggle NeoTree floating" },
-		c = { "<cmd>Neotree close<cr>", "Close NeoTree window" },
-    b = { "<cmd>Neotree toggle buffers float<cr>", "NeoTree show open buffers"},
-    g = { "<cmd>Neotree toggle git_status float<cr>", "NeoTree git"},
-	},
+ 	E = { "<cmd>NvimTreeToggle<CR>", "Nvim-Tree" },
+ 	e = {
+ 		name = "Nvim-Tree",
+ 		r = { "<cmd>NvimTreeRefresh<CR>", "Refresh Nvim-Tree" },
+ 		o = { "<cmd>NvimTreeOpen<CR>", "Open Nvim-Tree window" },
+ 		t = { "<cmd>NvimTreeToggle<CR>", "Toggle Nvim-Tree" },
+ 		f = { "<cmd>NvimTreeFocus<CR>", "Focus file in Nvim-Tree" },
+ 		c = { "<cmd>NvimTreeClose<CR>", "Close Nvim-Tree window" },
+ 		C = { "<cmd>NvimTreeClipboard<CR>", "Show Nvim-Tree clipboard" },
+ 	},
 
 	-- Terminal
 	T = {
 		name = "Terminal",
-    t = { "<cmd>lua _HTOP_TOGGLE()<cr>", "Htop" },
-    l = { "<cmd>lua _LAZYGIT_TOGGLE()<cr>", "Lazygit" },
-    n = { "<cmd>lua _NCDU_TOGGLE()<cr>", "Ncdu" },
-    f = { "<cmd>ToggleTerm direction=float<cr>", "Float" },
-    h = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", "Horizontal" },
-    v = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", "Vertical" },
+    ["1"] = { "<cmd>1ToggleTerm<CR>", "1" },
+    ["2"] = { "<cmd>2ToggleTerm<CR>", "2" },
+    ["3"] = { "<cmd>3ToggleTerm<CR>", "3" },
+    ["4"] = { "<cmd>4ToggleTerm<CR>", "4" },
+    t = { "<cmd>lua _HTOP_TOGGLE()<CR>", "Htop" },
+    l = { "<cmd>lua _LAZYGIT_TOGGLE()<CR>", "Lazygit" },
+    n = { "<cmd>lua _NCDU_TOGGLE()<CR>", "Ncdu" },
+    f = { "<cmd>ToggleTerm direction=float<CR>", "Float" },
+    h = { "<cmd>ToggleTerm size=10 direction=horizontal<CR>", "Horizontal" },
+    v = { "<cmd>ToggleTerm size=80 direction=vertical<CR>", "Vertical" },
 	},
 
-  -- Deletion
-	d = { "\"+d", "Copy deletion into register \"" },
-	D = { "\"+D", "Copy deletion to end into register \"" },
-	y = { "\"+y", "Copy yank into register \"" },
-	x = { "\"*d", "Copy char deletion into register \"" },
+
+  -- Debug
+   d = {
+     name = "Debug",
+     b = { "<cmd>lua require'dap'.toggle_breakpoint()<CR>", "Breakpoint" },
+     c = { "<cmd>lua require'dap'.continue()<CR>", "Continue" },
+     i = { "<cmd>lua require'dap'.step_into()<CR>", "Into" },
+     o = { "<cmd>lua require'dap'.step_over()<CR>", "Over" },
+     O = { "<cmd>lua require'dap'.step_out()<CR>", "Out" },
+     r = { "<cmd>lua require'dap'.repl.toggle()<CR>", "Repl" },
+     l = { "<cmd>lua require'dap'.run_last()<CR>", "Last" },
+     u = { "<cmd>lua require'dapui'.toggle()<CR>", "UI" },
+     x = { "<cmd>lua require'dap'.terminate()<CR>", "Exit" },
+   },
+
+  -- Config file
+	[0] = {
+    name = "Configuration Files",
+    s = {
+      "<cmd>source $NVIMDOTDIR/init.lua<CR> <bar> <cmd>lua require(\"notify\")(\" Config file sourced\", \"Info\")<cr>",
+      "Source Neovim config file"
+    },
+    e = { "<cmd>edit $NVIMDOTDIR/init.lua<CR>", "Edit Neovim config file" },
+    S = {
+      "<cmd>source $NVIMDOTDIR/lua/snippets<CR> <bar> <cmd>lua require(\"notify\")(\" Snippets file sourced\", \"Info\")<cr>",
+      "Reload snippet file"
+    },
+    ["%"] = { "<cmd>source %<CR>", "Source current buffer" },
+  }
 }
 
 local nosilent_maps = {
@@ -298,12 +362,6 @@ local nosilent_maps = {
       "Git add current file and commit"
     },
 	},
-	[0] = {
-    name = "Configuration File",
-    s = { "<cmd>source $NVIMDOTDIR/init.lua<cr>", "Source Neovim config file" },
-    e = { "<cmd>edit $NVIMDOTDIR/init.lua<cr>", "Edit Neovim config file" },
-    S = { "<cmd>source $NVIMDOTDIR/lua/user/cmp.lua<cr>", "Reload snippet file" },
-  },
 }
 
 local vopts = {
@@ -321,12 +379,12 @@ local vmappings = {
 	-- SURROUND
 	s = {
     name = "Surround",
-    ["."] = { "<cmd>lua require('surround').repeat_last()<cr>", "Repeat" },
-    a = { "<cmd>lua require('surround').surround_add(true)<cr>", "Add" },
-    d = { "<cmd>lua require('surround').surround_delete()<cr>", "Delete" },
-    r = { "<cmd>lua require('surround').surround_replace()<cr>", "Replace" },
-    q = { "<cmd>lua require('surround').toggle_quotes()<cr>", "Quotes" },
-    b = { "<cmd>lua require('surround').toggle_brackets()<cr>", "Brackets" },
+    ["."] = { "<cmd>lua require('surround').repeat_last()<CR>", "Repeat" },
+    a = { "<cmd>lua require('surround').surround_add(true)<CR>", "Add" },
+    d = { "<cmd>lua require('surround').surround_delete()<CR>", "Delete" },
+    r = { "<cmd>lua require('surround').surround_replace()<CR>", "Replace" },
+    q = { "<cmd>lua require('surround').toggle_quotes()<CR>", "Quotes" },
+    b = { "<cmd>lua require('surround').toggle_brackets()<CR>", "Brackets" },
   },
   d = { "\"+d", "Copy deletion into register \"" },
 	D = { "\"+D", "Copy deletion to end into register \"" },
@@ -345,9 +403,9 @@ local Zopts = {
 
 
 local Zmappings = {
-	Z = { ":update<cr>:Bdelete<cr>:bnext<cr>", "Save and Close buffer" },
-	Q = { "<cmd>quit!<cr>", "Close buffer and go to next" },
-	A = { ":%bdelete<cr>:Alpha<CR>", "Close all Buffers" },
+	Z = { "<cmd>update<CR> <cmd>Bdelete<CR> <cmd>bnext<CR>", "Save and Close buffer" },
+	Q = { "<cmd>quit!<CR>", "Close buffer and go to next" },
+	A = { "<cmd>bdelete<CR> <cmd>Alpha<CR>", "Close all Buffers" },
 }
 
 
@@ -378,6 +436,7 @@ local ins_mappings = {
 
 which_key.setup(setup)
 which_key.register(leader_maps, opts)
+which_key.register(nonLeader_maps, noLeader_opts)
 which_key.register(nosilent_maps, nosilent_opts)
 which_key.register(vmappings, vopts)
 which_key.register(Zmappings, Zopts)
