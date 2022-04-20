ddd50b465912a209a4c2e8da160d1f8f3cab069e
kevinm6 <72861758+kevinm6@users.noreply.github.com>
2022-04-20 12:57:45 +0200

    improve statusline functions and behavior

--- lua/user/statusline.lua
+++ lua/user/statusline.lua
@@ -1,50 +1,52 @@
- -------------------------------------
- -- File         : statusline.lua
- -- Description  : StatusLine config
- -- Author       : Kevin
- -- Source       : https://github.com/kevinm6/nvim/blob/nvim/lua/user/statusline.lua
- -- Last Modified: 12/03/2022 - 17:34
- -------------------------------------
-
+-------------------------------------
+-- File         : statusline.lua
+-- Description  : StatusLine config
+-- Author       : Kevin
+-- Source       : https://github.com/kevinm6/nvim/blob/nvim/lua/user/statusline.lua
+-- Last Modified: 19/04/2022 - 19:07
+-------------------------------------
 
 local ok, gps = pcall(require, "nvim-gps")
-if not ok then return end
+if not ok then
+	return
+end
 
-local icons = require "user.icons"
+local icons = require("user.icons")
 
-local trunc_width = setmetatable({
+local max_width = setmetatable({
 	filename = 120,
 	git_status = 90,
-	diagnostic = 130,
+	diagnostic = 90,
 	row_onTot = 60,
 	gps_loc = 80,
 }, {
 	__index = function()
 		return 80
-	end
+	end,
 })
 
-
-local function is_truncated(width)
-	local current_width = vim.api.nvim_win_get_width(0)
-	return current_width < width
+local is_truncated = function(width)
+	return vim.api.nvim_win_get_width(0) < width
 end
 
-
-local function get_filename()
-	if is_truncated(trunc_width.filename) then return "%t" end
-	return "%<%f"
+local get_filename = function()
+	if is_truncated(max_width.filename) then
+		return "%t"
+	end
+	return "%<%t"
 end
 
-
-local function get_line_onTot()
-	if is_truncated(trunc_width.row_onTot) then return " %2*%l%3*÷%L " end
+local get_line_onTot = function()
+	if is_truncated(max_width.row_onTot) then
+		return " %2*%l%3*÷%L "
+	end
 	return " row %2*%l%3*÷%L "
 end
 
-
-local function get_lsp_diagnostic()
-	if is_truncated(trunc_width.diagnostic) then return "" end
+local get_lsp_diagnostic = function()
+	if is_truncated(max_width.diagnostic) then
+		return ""
+	end
 
 	local diagnostics = vim.diagnostic.get(0)
 	local count = { 0, 0, 0, 0 }
@@ -58,88 +60,75 @@ local function get_lsp_diagnostic()
 	local infos = icons.diagnostics.Warning .. ":" .. (count[vim.diagnostic.severity.INFO] or 0)
 	local hints = icons.diagnostics.Hint .. ":" .. (count[vim.diagnostic.severity.HINT] or 0)
 
-	return string.format(
-		 "%s %s %s %s",
-		 errors, warnings, infos, hints)
+	return string.format("%s %s %s %s", errors, warnings, infos, hints)
 end
 
-
-local function get_git_status()
-	local signs = vim.b.gitsigns_status_dict or {head = "", added = 0, changed = 0, removed = 0}
+local get_git_status = function()
+	local signs = vim.b.gitsigns_status_dict or { head = "", added = 0, changed = 0, removed = 0 }
 	local is_head_empty = signs.head ~= ""
 
-	if is_truncated(trunc_width.git_status) then
-		return is_head_empty and string.format(
-       " %s ", signs.head or ""
-     ) .. '%1*' .. icons.ui.SlChevronRight .. " " or ""
+	if is_truncated(max_width.git_status) then
+		return is_head_empty
+				and string.format(" %s ", signs.head or "") .. "%1*" .. icons.ui.SlChevronRight .. " "
+			or ""
 	end
 
 	return is_head_empty
-		and string.format(
-      "+%s ~%s -%s |  %s ",
-			signs.added, signs.changed, signs.removed, signs.head
-		) .. '%1*' .. icons.ui.SlChevronRight .. " " or ""
+			and string.format("+%s ~%s -%s |  %s ", signs.added, signs.changed, signs.removed, signs.head) .. "%1*" .. icons.ui.SlChevronRight .. " "
+		or ""
 end
 
-
-local	function nvim_gps()
-	if (not gps.is_available()) or (is_truncated(trunc_width.gps_loc)) then return "" end
-	return gps.get_location()
+local nvim_gps = function()
+	if gps.is_available() and (not is_truncated(max_width.gps_loc)) then
+		return gps.get_location()
+	else
+		return ""
+	end
 end
 
-
-Statusline = {}
-
-Statusline.active = function()
+_G.active = function()
 	-- LeftSide
-	local bufN = '%1*%n' .. icons.ui.SlChevronRight
-	local git = '%2*'.. get_git_status()
-	local fname = '%6*' .. get_filename() .. '%m '
-	local endLeftSide =	'%4*' .. icons.ui.SlArrowRight
+	local bufN = "%#User1#%n" .. icons.ui.SlChevronRight
+	local git = "%#User2#" .. get_git_status()
+	local fname = "%#User6#" .. get_filename() .. "%m "
+	local endLeftSide = "%#User4#" .. icons.ui.SlArrowRight
 	-- Center & separators
-  local gps_out = '%5*' .. nvim_gps() .. '%m'
-	local sideSep = '%='
+	local gps_out = "%#User5#" .. nvim_gps() .. "%m"
+	local sideSep = "%="
+  local lsp_diag =  get_lsp_diagnostic()
 	-- RightSide
-	local endRightSide = '%4*' .. icons.ui.SlArrowLeft
-	local fencoding = '%3* %{&fileencoding?&fileencoding:&encoding}'
-	local ftype = '%1* %y'
-	local fformat = '%3* ' .. icons.ui.SlChevronLeft .. ' %{&ff}'
+	local endRightSide = "%#User4#" .. icons.ui.SlArrowLeft
+	local fencoding = "%#User3# %{&fileencoding?&fileencoding:&encoding}"
+	local ftype = "%#User1# %y"
+	local fformat = "%#User3# " .. icons.ui.SlChevronLeft .. " %{&ff}"
 
 	return table.concat({
 		-- Left Side
-		bufN, " ", git, fname, endLeftSide, " ", gps_out,
+		bufN,
+		" ",
+		git,
+		fname,
+		endLeftSide,
+		" ",
+		gps_out,
 		-- Center & separators
 		sideSep,
-		get_lsp_diagnostic(), " ",
+		lsp_diag,
+		" ",
 		-- Right Side
-		endRightSide, fencoding, ftype,
-		fformat, " ", icons.ui.SlChevronLeft .. get_line_onTot()
+		endRightSide,
+		fencoding,
+		ftype,
+		fformat,
+		" ",
+		icons.ui.SlChevronLeft .. get_line_onTot(),
 	})
 end
 
-
-function Statusline.inactive()
-	return "%= %f %="
-end
-
-
-function Statusline.explorer()
+_G.explorer = function()
 	return "%= File Explorer %="
 end
 
-
-function Statusline.dashboard()
+_G.dashboard = function()
 	return "%= Dashboard %="
 end
-
-
-vim.api.nvim_exec([[
-	augroup Statusline
-    au!
-    au WinEnter,BufEnter * setlocal statusline=%!v:lua.Statusline.active()
-    au WinLeave,BufLeave * setlocal statusline=%!v:lua.Statusline.inactive()
-    au FileType,BufEnter,WinEnter neo-tree setlocal statusline=%!v:lua.Statusline.explorer()
-    au FileType,BufEnter,WinEnter alpha setlocal statusline=%!v:lua.Statusline.dashboard()
-	augroup END
-]], false)
-
