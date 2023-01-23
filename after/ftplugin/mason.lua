7cf86fd9332ea3a7bb05b10e130bde28a88b73e7
kevinm6 <72861758+kevinm6@users.noreply.github.com>
2023-01-23 10:44:03 +0100

    refactor: move mason settings from ftplugin to mason config

--- after/ftplugin/mason.lua
+++
@@ -1,1 +0,0 @@
-vim.cmd [[resize -2]]
--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -2,7 +2,7 @@
 -- File         : init.lua
 -- Description  : config all module to be imported
 -- Author       : Kevin
--- Last Modified: 21 Jan 2023, 18:39
+-- Last Modified: 23 Jan 2023, 09:29
 -------------------------------------
 
 local icons = require "user.icons"
@@ -24,56 +24,58 @@ local M = {
       event = "BufReadPre",
       keys = {
         { "<leader>M", vim.cmd.Mason, desc = "Mason" }
-    },
-    opts = {
-      install_root_dir = vim.fn.stdpath "data" .. "/mason",
-      PATH = "append",
+      },
+      opts = {
+        install_root_dir = vim.fn.stdpath "data" .. "/mason",
+        PATH = "append",
 
-      ui = {
-        border = "rounded",
-        icons = {
-          -- The list icon to use for installed servers.
-          package_installed = icons.packer.done_sym,
-          -- The list icon to use for servers that are pending installation.
-          package_pending = icons.packer.working_sym,
-          -- The list icon to use for servers that are not installed.
-          package_uninstalled = icons.packer.removed_sym,
+        ui = {
+          border = "rounded",
+          width = 0.7,
+          height = 0.7,
+          icons = {
+            -- The list icon to use for installed servers.
+            package_installed = icons.packer.done_sym,
+            -- The list icon to use for servers that are pending installation.
+            package_pending = icons.packer.working_sym,
+            -- The list icon to use for servers that are not installed.
+            package_uninstalled = icons.packer.removed_sym,
+          },
+          keymaps = {
+            -- Keymap to expand a server in the UI
+            toggle_package_expand = "<CR>",
+            -- Keymap to install the server under the current cursor position
+            install_package = "i",
+            -- Keymap to reinstall/update the server under the current cursor position
+            update_server = "u",
+            -- Keymap to check for new version for the server under the current cursor position
+            check_package_version = "c",
+            -- Keymap to update all installed servers
+            update_all_packages = "U",
+            -- Keymap to check which installed servers are outdated
+            check_outdated_packages = "C",
+            -- Keymap to uninstall a server
+            uninstall_package = "D",
+            -- Keymap to cancel a package installation
+            cancel_installation = "<C-c>",
+            -- Keymap to apply language filter
+            apply_language_filter = "<C-f>",
+          },
         },
-        keymaps = {
-          -- Keymap to expand a server in the UI
-          toggle_package_expand = "<CR>",
-          -- Keymap to install the server under the current cursor position
-          install_package = "i",
-          -- Keymap to reinstall/update the server under the current cursor position
-          update_server = "u",
-          -- Keymap to check for new version for the server under the current cursor position
-          check_package_version = "c",
-          -- Keymap to update all installed servers
-          update_all_packages = "U",
-          -- Keymap to check which installed servers are outdated
-          check_outdated_packages = "C",
-          -- Keymap to uninstall a server
-          uninstall_package = "D",
-          -- Keymap to cancel a package installation
-          cancel_installation = "<C-c>",
-          -- Keymap to apply language filter
-          apply_language_filter = "<C-f>",
+        pip = {
+          install_args = {},
         },
-      },
-      pip = {
-        install_args = {},
-      },
-      max_concurrent_installers = 3,
-      github = {
-        -- The template URL to use when downloading assets from GitHub.
-        -- The placeholders are the following (in order):
-        -- 1. The repository (e.g. "rust-lang/rust-analyzer")
-        -- 2. The release version (e.g. "v0.3.0")
-        -- 3. The asset name (e.g. "rust-analyzer-v0.3.0-x86_64-unknown-linux-gnu.tar.gz")
-        download_url_template = "https://github.com/%s/releases/download/%s/%s",
-      },
-      log_level = vim.log.levels.INFO,
-    }
+        max_concurrent_installers = 3,
+        github = {
+          -- The template URL to use when downloading assets from GitHub.
+          -- The placeholders are the following (in order):
+          -- 1. The repository (e.g. "rust-lang/rust-analyzer")
+          -- 2. The release version (e.g. "v0.3.0")
+          -- 3. The asset name (e.g. "rust-analyzer-v0.3.0-x86_64-unknown-linux-gnu.tar.gz")
+          download_url_template = "https://github.com/%s/releases/download/%s/%s",
+        },
+        log_level = vim.log.levels.INFO,
+      }
     },
     {
       "williamboman/mason-lspconfig.nvim",
@@ -166,22 +168,22 @@ function M.config()
     -- Next, you can provide targeted overrides for specific servers.
     -- Manage server with custom setup
     ["sumneko_lua"] = function() lspconfig.sumneko_lua.setup(vim.tbl_deep_extend("force", default_lsp_config,
-        require "plugins.lsp.configs.sumneko_lua"))
+      require "plugins.lsp.configs.sumneko_lua"))
     end,
     ["jsonls"] = function() lspconfig.jsonls.setup(vim.tbl_deep_extend("force", default_lsp_config,
-        require "plugins.lsp.configs.jsonls"))
+      require "plugins.lsp.configs.jsonls"))
     end,
     ["sqls"] = function() lspconfig.sqls.setup(vim.tbl_deep_extend("force", default_lsp_config,
-        require "plugins.lsp.configs.sqls"))
+      require "plugins.lsp.configs.sqls"))
     end,
     ["grammarly"] = function() lspconfig.grammarly.setup(vim.tbl_deep_extend("force", default_lsp_config,
-        { autostart = false }))
+      { autostart = false }))
     end,
     ["clangd"] = function() lspconfig.clangd.setup(vim.tbl_deep_extend("force", default_lsp_config,
-        require "plugins.lsp.configs.clangd"))
+      require "plugins.lsp.configs.clangd"))
     end,
     ["gopls"] = function() lspconfig.gopls.setup(vim.tbl_deep_extend("force", default_lsp_config,
-        require "plugins.lsp.configs.gopls"))
+      require "plugins.lsp.configs.gopls"))
     end,
   }
 
