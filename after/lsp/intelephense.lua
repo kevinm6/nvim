475afc406221f17aefc931e7697007481d434a3a
kevinm6 <72861758+kevinm6@users.noreply.github.com>
2026-04-22 18:46:32 +0200

    chore: improve php lsp config

--- after/lsp/intelephense.lua
+++ after/lsp/intelephense.lua
@@ -2,10 +2,37 @@
 return {
   cmd = { "intelephense", "--stdio" },
   filetypes = { "php" },
-  init_options = {
-    globalStoragePath = vim.fn.expand "~/.local/php/",
-    -- clearCache = true
-    root_markers = { "composer.json", ".git" },
+  root_markers = { "composer.json", ".git" },
+  settings = {
+    intelephense = {
+      telemetry = { enabled = false },
+      -- completion = { parameterCase = "camel" },
+      format = {
+        enable = true,
+        braces = "k&r", --default: per
+      },
+      editor = {
+        tabSize = 2,
+        insertSpaces = true
+      },
+      phpdoc = { addPackage = true },
+      trace = { server = "messages" },
+      stubs = {
+        "bcmath", "bz2", "Core", "curl", "date", "dom", "fileinfo", "filter", "gd", "gettext", "hash", "iconv", "imap",
+        "intl", "json", "libxml", "mbstring", "mcrypt", "mysql", "mysqli", "password", "pcntl", "pcre", "PDO",
+        "pdo_mysql", "Phar", "readline", "regex", "session", "SimpleXML", "sockets", "sodium", "standard", "superglobals",
+        "tokenizer", "xml", "xdebug", "xmlreader", "xmlwriter", "yaml", "zip", "zlib", "wordpress-stubs",
+        "woocommerce-stubs", "acf-pro-stubs", "wordpress-globals", "wp-cli-stubs", "genesis-stubs", "polylang-stubs",
+        "bcmath", "bz2", "calendar", "Core", "curl", "zip", "zlib", "wordpress", "md5",
+        "woocommerce", "acf-pro", "wordpress-globals", "wp-cli", "genesis", "polylang"
+      },
+      environemnt = {
+        includePaths = { "vendor/php-stubs" },
+      },
+      files = {
+        maxSize = 5000000,
+      }
+    }
   },
   docs = {
     description = [[
